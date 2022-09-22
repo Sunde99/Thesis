279ef015c4532590d4d2d13e2f67bd6e59ff030e
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2022-09-22 14:11:05 +0200

    tweaks lighting

--- src/lights/ambientLights.ts
+++ src/lights/ambientLights.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 
 export const createAmbientLights = (scene: THREE.Scene) => {
   const ambientLight = new THREE.AmbientLight(0x404040) // soft white light
+  ambientLight.intensity = 0.1
   scene.add(ambientLight)
   return ambientLight
 }
