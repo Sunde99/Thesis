19120d0f9c6f2dcd3e1f9dfbdc6709eaaee7a53d
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2022-09-22 14:11:22 +0200

    Adds randomly placed objects

--- src/submergedObject.ts
+++ src/submergedObject.ts
@@ -1,35 +1,65 @@
 import * as THREE from 'three'
+import { Mesh, BufferGeometry, Material } from 'three'
 
-const loadSubmergedObject = (): THREE.Mesh => {
-  // TODO Set up object loader
-  // For now using a cube is ok
+const createSphere = (): THREE.Mesh => {
+  const sphereGeometry = new THREE.SphereGeometry(10, 32, 16)
+  const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0x090fff })
+  let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+  return sphere
+}
 
-  // Creates the geometry + materials
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshPhongMaterial({ color: 0xff9900 })
-  let cube = new THREE.Mesh(geometry, material)
+const createBox = (): THREE.Mesh => {
+  const boxGeometry = new THREE.BoxGeometry(10, 10, 10)
+  const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xff9900 })
+  let cube = new THREE.Mesh(boxGeometry, boxMaterial)
   return cube
 }
 
-export const getSubmergedObject = () => {
-  const submergedObject = loadSubmergedObject()
-  submergedObject.position.y = 0.5
+const loadSubmergedObjects = (): THREE.Mesh[] => {
+  // TODO Set up object loader
+  // For now using a basic shapes is ok
+  const submergedObjects: THREE.Mesh[] = []
+  const amountOfObjects = 5 // Take this as param
+
+  // Randomly creates the geometry + materials
+  for (let i = 0; i < amountOfObjects; i++) {
+    const randomNum = Math.floor(Math.random() * 2)
+    if (randomNum == 1) {
+      submergedObjects.push(createBox())
+    } else {
+      submergedObjects.push(createSphere())
+    }
+  }
+
+  return submergedObjects
+}
+
+export const placeSubmergedObjects = () => {
+  const submergedObjects = loadSubmergedObjects()
 
   // Sets up outer bounds
-  const maxXValue = 5
-  const minXValue = -5
-  const maxZValue = 5
-  const minZValue = -5
+  const minXValue = -75
+  const maxXValue = 75
+  const minYValue = -1
+  const maxYValue = 30
+  const minZValue = -75
+  const maxZValue = 75
 
-  // Randomizes position and rotation
-  const submergedObjectRotation = Math.random() * Math.PI * 2
-  submergedObject.position.x =
-    Math.random() * (maxXValue - minXValue) + minXValue
-  submergedObject.position.z =
-    Math.random() * (maxZValue - minZValue) + minZValue
-  submergedObject.rotation.setFromVector3(
-    new THREE.Vector3(0, submergedObjectRotation, 0),
-  )
+  submergedObjects.forEach((submergedObject) => {
+    submergedObject.position.y = 0.5
 
-  return submergedObject
+    // Randomizes position and rotation
+    const submergedObjectRotation = Math.random() * Math.PI * 2
+    submergedObject.position.x =
+      Math.random() * (maxXValue - minXValue) + minXValue
+    submergedObject.position.y =
+      Math.random() * (maxYValue - minYValue) + minYValue
+    submergedObject.position.z =
+      Math.random() * (maxZValue - minZValue) + minZValue
+    submergedObject.rotation.setFromVector3(
+      new THREE.Vector3(0, submergedObjectRotation, 0),
+    )
+  })
+
+  return submergedObjects
 }
