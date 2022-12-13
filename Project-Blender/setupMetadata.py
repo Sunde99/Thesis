a40d258d73786df66baa315fac934c5437d51654
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2022-12-13 12:40:20 +0100

    Generalizes paths

--- Project-Blender/setupMetadata.py
+++ Project-Blender/setupMetadata.py
@@ -1,5 +1,14 @@
 import csv
+import sys
+import os
 import itertools
+import bpy
+
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir )
+print("dir: " + str(dir))
+
 
 def setupMetadata():
     header = ['ID', 'flipColors', 'GroundTexture', 'GroundPrimaryColor', 'GroundSecondaryColor', 'LightX', 'LightY', 'LightZ', 'WaterHeight', 'LegoX', 'LegoY', 'legoRot']
@@ -14,9 +23,9 @@ def setupMetadata():
     LegoX = [-0.700]
     LegoY = [-0.710]
     LegoRot = [2.69, 1.67]
-    temp = [groundTextures, flipColors, groundPrimaryColors, groundSecondaryColors, lightX, lightY, lightZ, WaterHeight, LegoX, LegoY, LegoRot]
+    temp = [flipColors, groundTextures, groundPrimaryColors, groundSecondaryColors, lightX, lightY, lightZ, WaterHeight, LegoX, LegoY, LegoRot]
     everyCombination = list(itertools.product(*temp))
-    with open('C:\\Users\\Matias\\_Thesis\\pictures\\Metadata\\ImageData.csv', 'w', encoding='UTF8', newline='') as f:
+    with open(f'{dir}\\pictures\\Metadata\\ImageData.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         i = 0
